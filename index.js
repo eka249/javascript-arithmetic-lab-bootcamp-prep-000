27d97bd0a8d92e64e5fa5ddf10258423eb1bf609
Ellyn Anderson <eka249@nyu.edu>
2019-05-08 07:14:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,23 @@ var b= 3
 function add(a,b){
   return a+b
   }
-add()
-subtract(n,3)
-multiply (n,4)
-inc(n, 1)
+function subtract(a,b){
+  return a-b
+}
+
+function multiply(a,b){
+  return a*b
+}
+function divide(a,b){
+  return a/b
+}
+
+function inc(n){
+  return n++
+}
 
 makeInt(n)
 preserveDecimal(n,.10)
+add()
+subtract(n,3)
+multiply (n,4)
